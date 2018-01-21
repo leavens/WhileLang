7ec851373f4f54308fa62d2ebe7068f36c443374
Gary T. Leavens <leavens@eecs.ucf.edu>
2018-01-21 17:54:13 -0500

    Adding the new files, actually

---
+++ edu.ucf.cs.whilelang.WhileLang.ui.tests/src/edu/ucf/cs/whilelang/WhileLang/ui/tests/WhileLangContentAssistTest.xtend
@@ -0,0 +1,13 @@
+package edu.ucf.cs.whilelang.WhileLang.ui.tests
+
+import org.junit.Assert
+import org.junit.Test
+
+/** So far this is just a stub. */
+class WhileLangContentAssistTest {
+    
+    @Test
+    def void itsOK() {
+        Assert.assertTrue(true)
+    }
+}
---
+++ edu.ucf.cs.whilelang.WhileLang.ui.tests/xtend-gen/edu/ucf/cs/whilelang/WhileLang/ui/tests/WhileLangContentAssistTest.java
@@ -0,0 +1,15 @@
+package edu.ucf.cs.whilelang.WhileLang.ui.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * So far this is just a stub.
+ */
+@SuppressWarnings("all")
+public class WhileLangContentAssistTest {
+  @Test
+  public void itsOK() {
+    Assert.assertTrue(true);
+  }
+}
